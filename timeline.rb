0670e1ee4a019a9c6b2a39281f01dc43aa04189e
Paul Davis <net.prophet.nz@gmail.com>
2008-06-03 17:21:42 +1200

    Can't use yaml or std libs. Backup.

--- timeline.rb
+++ timeline.rb
@@ -5,20 +5,21 @@
 #  http://dl.google.com/sketchup/gsu6/GoogleSketchUpMEN.dmg
 #  http://sketchup.google.com/download/rubyscripts.html
 #  http://code.google.com/apis/sketchup/docs/developers_guide/index.html
+#  http://groups.google.com/group/SketchUp-Plugins-Dev/web/Ruby-classes.html
 #
 #=============================================================================
 
-#require 'sketchup.rb' 
-require 'yaml' # http://www.yaml.org/
+require 'sketchup.rb' 
+Sketchup.require('/System/Library/Frameworks/Ruby.framework/Versions/1.8/usr/lib/ruby/1.8/yaml') # http://www.yaml.org/
 
 #=============================================================================
 
 class TimeLine
 
-  attr_accessor :name, :birth_place, :birth_date, :locations, :schools, :jobs, :events
+  attr_accessor :name, :birth_date, :locations, :schools, :jobs, :events
 
   def to_s
-    "Timeline for: #{@name}, born: #{@birth_date.to_s} in #{@birth_place}" << 
+    "Timeline for: #{@name}, born: #{@birth_date.to_s}" << 
       "\n" << @locations.to_s << @schools.to_s # << @jobs.to_s << @events.to_s
   end
   
@@ -26,14 +27,19 @@ class TimeLine
     File.open(filename, "w") {|f| YAML.dump(self, f)}
   end
   
+  def draw
+    # draw all elements into a 5 x decade coordinate system....
+    Sketchup.active_model.add_note @name, 0.1, 0.1
+  end
+  
 end
 
 class TLLocation
 
-  attr_accessor :name, :date
+  attr_accessor :name, :start, :finish
   
   def to_s
-    "  Location: #{@name} from: #{@date.to_s}\n"
+    "  Location: #{@name} from: #{@start.to_s} to #{@finish.to_s}\n"
   end
 
 end
@@ -59,21 +65,22 @@ end
 
 #=============================================================================
 if __FILE__ == $0 # running from command line to test
-  tl = YAML.load(File.open("timeline.yaml"))
+  tl = YAML.load(File.open("timeline.pd.yaml"))
   puts tl.to_yaml
 end
 
 #=============================================================================
-# Add a menu to draw timeline into SketchUp (tested)
-# if( not $timeline_menu_loaded )
-#     plugins_menu = UI.menu("Plugins")
-#     tl_menu = plugins_menu.add_submenu("Time Line")
-#     tl_menu.add_item("Load File") { load "timeline.dat" }
-#     dr_menu = tl_menu.add_submenu("Draw")
-#     #...
-#     dr_menu.add_item("Location") { puts "Draw Location Item" }
-#     dr_menu.add_item("School") { puts "Draw School Item" }
-#     #...
-#     $timeline_menu_loaded = true
-# end
+#Add a menu to draw timeline into SketchUp (tested)
+if( not $timeline_menu_loaded )
+    plugins_menu = UI.menu("Plugins")
+    tl_menu = plugins_menu.add_submenu("Time Line")
+    tl_menu.add_item("Load File") { tl = YAML.load(File.open("timeline.pd.yaml")) }
+    tl_menu.add_item("Draw") { tl.draw }
+    #dr_menu = tl_menu.add_submenu("Draw")
+    #...
+    #dr_menu.add_item("Location") { puts "Draw Location Item" }
+    #dr_menu.add_item("School") { puts "Draw School Item" }
+    #...
+    $timeline_menu_loaded = true
+end
 
